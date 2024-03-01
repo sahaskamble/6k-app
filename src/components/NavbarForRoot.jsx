25319c68166379d673fbaf5472d5ff7f802869ba
shashanksangawar <shashanksangawar.bit@gmail.com>
2024-03-01 06:17:18 +0530

    Hi

--- src/components/NavbarForRoot.jsx
+++ src/components/NavbarForRoot.jsx
@@ -6,6 +6,13 @@ const Navbar = () => {
 
   useEffect(() => {
 
+    const btn = document.getElementById('navbar_menu_button');
+    const menu = document.getElementById('navbar_float_menu');
+
+    btn.addEventListener('click', () => {
+      menu.classList.toggle("hidden");
+    })
+
   }, []);
 
   return (
@@ -18,11 +25,13 @@ const Navbar = () => {
         </div>
 
         <div className="navbar_menu text-lg flex items-center gap-8">
-          <a href="/application" ><span className="navbar_items">Application</span></a>
-          <a href="/login" ><span className="navbar_items"><i className="bi bi-box-arrow-right"></i> Sign Up</span></a>
+          <span className="navbar_items">Application</span>
+          <a href="/root/university" className="navbar_items">University</a>
+          <a href="/root/country" className="navbar_items">Country</a>
+          <a href="/root/courses" className="navbar_items">Courses</a>
         </div>
 
-        {/*<div id="navbar_float_menu" className="hidden absolute top-[9dvh] right-3 w-[300px] h-[250px] py-3 rounded-lg bg-[--bg]">
+        <div id="navbar_float_menu" className="hidden absolute top-[9dvh] right-3 w-[300px] h-[250px] py-3 rounded-lg bg-[--bg]">
           <div className="flex h-full flex-col justify-between items-center px-2 text-xl " >
             <div className="container flex justify-between items-center gap-8 py-4">
               <div className="w-[40%] flex justify-center items-center">
@@ -39,7 +48,7 @@ const Navbar = () => {
         <div id="navbar_menu_button" className="navbar_menu_button text-2xl flex items-center gap-2 bg-[--color2] px-3 py-1 rounded-full cursor-pointer">
           <i className="bi bi-list"></i>
           <img id="image" className="w-[25px] h-[25px] rounded-full border-[2px] border-[#1d1d1b]" src={img1.src} alt="profile pic" />
-        </div>*/}
+        </div>
 
       </nav>
 
